d033862f4022a04072c8d09b3c9ddb427e9dc2bf
Emily Déat <emily.deat@gmail.com>
2020-03-25 16:20:21 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Claire'));
+console.log(chalk.red('Auxence'));
+console.log(chalk.green('Aurélien'));
+console.log(chalk.yellow('Tada'));
+
